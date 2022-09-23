fa78d033676a5b74ddc6ffbdc41becc7e8fcb39f
Moon sheikh <85309683+moonsheikh59@users.noreply.github.com>
2022-09-23 16:41:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from typing import Text
-
-
 print("\t\t *''WELLCOME''*\t\t")
-# print(ord("Z"))
-# print(chr(90))
-# print(chr(65))
-#print(100%101)
 def encryption(text,shifts):
     encrypted = ""
-
     for i in text:
         if i == " ":
-        #+= means encrypted = encrypted+ " "
             encrypted += " "
         else:
-            #M = 77, A = 65///unicode value without shifting
-            # 77 - 65 = (12 = M in alpha) 
-            # 12 + 3 = (15 = P in alpha)//shifting in alpha
-            # 15 + 65 = 80(P)/////unicode value with shifting
             encrypted += chr((ord(i)-65 +shifts)%26+65)
-        
-
     return encrypted
 
 def decryption(text,shifts):
@@ -29,10 +15,8 @@ def decryption(text,shifts):
         if j == " ":
             encrypted += " "
         else:
-            encrypted += chr((ord(j)-65 -shifts)%26+65)
-        
+            encrypted += chr((ord(j)-65 -shifts)%26+65)        
     return encrypted
-
 
 get = input("What do you want to do ? \nEnter E for Encryptio:- \nEnter D for decryption:- ")
 #getting all Values in uppercase
@@ -45,4 +29,3 @@ elif get == "D" or get == "d":
     print("YOUR PLAIN TEXT IS :-",decryption(Text,shifts))
 else:
     print("you Entered invalid string")
-
